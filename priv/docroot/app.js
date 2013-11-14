1c8cfb7c1fdff2d64dc35eae3a12fa8bea69b366
Martin Wiso <martin@wiso.cz>
2013-11-14 17:27:40 +0100

    Remove commented out code block

--- priv/docroot/app.js
+++ priv/docroot/app.js
@@ -4,18 +4,3 @@ var dashboardApp = angular.module('dashboardApp', [
     'dashboardControllers'
   ]
 );
-/*
-.config(['$routeProvider',
-  function($routeProvider) {
-      console.log("app config");
-      $routeProvider.
-          when('/statman', {
-              controller: 'DashboardCtrl',
-              template
-          }).
-          otherwise({
-              redirectTo: '/statman'
-          });
-  }]
-);
-*/
