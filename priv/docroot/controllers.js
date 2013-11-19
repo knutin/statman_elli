05332e8312db1d41edb773a11e29c6a21fbc25aa
Martin Wiso <martin@wiso.cz>
2013-11-19 22:59:45 +0100

    Refactoring dashboard using custom directives

--- priv/docroot/controllers.js
+++ priv/docroot/controllers.js
@@ -4,7 +4,11 @@ var dashboardControllers = angular.module('dashboardControllers', []);
 dashboardApp.controller('DashboardCtrl', ["$scope",
    function DashboardCtrl($scope) {
 
-       $scope.histograms = [];
+       // table data definitions
+       $scope.ratesGrid = {rows: 'ratesRows', columns: 'ratesCols'};
+       $scope.gaugesGrid = {rows: 'gaugesRows', columns: 'gaugesCols'};
+       $scope.histNodesGrid = {rows: 'histNodesRows', columns: 'histCols'};
+       $scope.histMergedGrid = {rows: 'histMergedRows', columns: 'histCols'};
 
        // reload data from stream
        var stream = new EventSource("statman/stream");
@@ -14,26 +18,29 @@ dashboardApp.controller('DashboardCtrl', ["$scope",
            });
        }, false);
 
+       //TODO: update only what needs to be updated!!!
+       //TODO: replace underscore with angular module
        $scope.updateUI = function (data) {
            // general
            $scope.hostname = data.hostname;
 
            // rates
            var rates = $scope.getByType(data, "counter");
-           $scope.ratesNodes = $scope.getNodes(rates);
-           $scope.rates = $scope.postProcessData($scope.ratesNodes, rates);
+           var ratesNodes = $scope.getNodes(rates);
+           $scope.ratesCols = $scope.getNodeColumns(ratesNodes);
+           $scope.ratesRows = $scope.postProcessData(ratesNodes, rates);
 
            // gauges
            var gauges = $scope.getByType(data, "gauge");
-           $scope.gaugesNodes = $scope.getNodes(gauges);
-           $scope.gauges = $scope.postProcessData($scope.gaugesNodes, gauges);
+           var gaugesNodes = $scope.getNodes(gauges);
+           $scope.gaugesCols = $scope.getNodeColumns(gaugesNodes);
+           $scope.gaugesRows = $scope.postProcessData(gaugesNodes, gauges);
 
            // merged and node histagrams
            var histograms = $scope.getByType(data, "histogram");
-           $scope.histograms = $scope.histograms.concat(histograms);
-
-           $scope.mergedHistograms = $scope.postProcessHistograms($scope.histograms);
-           $scope.nodesHistograms = $scope.postProcessNodesHistograms($scope.histograms);
+           $scope.histCols = $scope.getHistogramColumns();
+           $scope.histNodesRows = $scope.postProcessNodesHistograms(histograms);
+           $scope.histMergedRows = $scope.postProcessHistograms(histograms);;
        };
 
        // helpers
@@ -43,14 +50,21 @@ dashboardApp.controller('DashboardCtrl', ["$scope",
            var result = [];
 
             _.each(keys, function (key) {
-                _.each(nodes, function (node) {
+                var row = new Object();
+                row.key = key;
+                _.each(nodes, function (node, i) {
                     var d = _.find(grouped[key], function (c) {
                         return c.node == node;
                     });
-                    if (d) {
-                        result.push({"key": key, "value": d.value});
+                    if(d) {
+                        row["node" + i] = $scope.formatNumber(
+                            (d.rate != undefined) ? d.rate : d.value
+                        );
+                    } else {
+                        row["node" + i] = "";
                     }
-                })
+                });
+                result.push(row);
            });
            return result;
        };
@@ -58,17 +72,28 @@ dashboardApp.controller('DashboardCtrl', ["$scope",
            var histograms = _.filter(data, function (h) {
                return h.node instanceof Array
            });
-           //console.dir(histograms);
            var grouped = _.groupBy(histograms, function (h) {
                return h.id;
            });
-           //console.dir(grouped);
            var result = [];
 
            _.each(grouped, function (grouped, i) {
-               //console.dir(grouped);
-               //var items = histograms[i];
-               //result.push({"histogram": histograms[i]['id'], "values": items});
+               var row = new Object();
+               row.label = grouped[0].id;;
+               row.items = [];
+               _.each(grouped, function (group) {
+                   row.items.push({
+                       "key":  group.key,
+                       "rate": $scope.formatNumber(group.rate),
+                       "observations": $scope.formatNumber(group.observations),
+                       "mean": $scope.formatMs(group.mean),
+                       "sd":   $scope.formatNumber(group.sd),
+                       "p95":  $scope.formatMs(group.p95),
+                       "p99":  $scope.formatMs(group.p99),
+                       "max":  $scope.formatMs(group.max)
+                   });
+               });
+               result.push(row);
            });
            return result;
        };
@@ -86,19 +111,52 @@ dashboardApp.controller('DashboardCtrl', ["$scope",
                    }),
                    function (h) { return h.id }
                );
-               _.each(nodes, function (node) {
-                   _.each(grouped, function (grouped, i) {
-                       var items = [];
-                       var label = node + ": " + i;
-                       _.each(grouped, function (g) {
-                           items.push(g);
+               _.each(grouped, function (grouped, i) {
+                   var row = new Object();
+                   row.label = (i == "null") ? node : node + i;
+                   row.items = [];
+                   _.each(grouped, function (group) {
+                       row.items.push({
+                           "key":  group.key,
+                           "rate": $scope.formatNumber(group.rate),
+                           "observations": $scope.formatNumber(group.observations),
+                           "mean": $scope.formatMs(group.mean),
+                           "sd":   $scope.formatNumber(group.sd),
+                           "p95":  $scope.formatMs(group.p95),
+                           "p99":  $scope.formatMs(group.p99),
+                           "max":  $scope.formatMs(group.max)
                        });
-                       result.push({"label": label, "items": items});
                    });
+                   result.push(row);
                });
            });
            return result;
        };
+
+       // columns builders
+       $scope.getNodeColumns = function(nodes) {
+           var columns = [{label: 'Key', field: 'key'}];
+           _.each(nodes, function(node, i) {
+               columns.push({label: node,
+                             field: "node" + i,
+                             headerClass: "alignRight"
+                            });
+           });
+           return columns;
+       };
+       $scope.getHistogramColumns = function() {
+           return [{label: ""}, {label: ""},
+                   {label: "Rate per second", headerClass: "alignRight"},
+                   {label: "Observations", headerClass: "alignRight"},
+                   {label: "Mean (ms)", headerClass: "alignRight"},
+                   {label: "Standard deviation", headerClass: "alignRight"},
+                   {label: "95th (ms)", headerClass: "alignRight"},
+                   {label: "99th (ms)", headerClass: "alignRight"},
+                   {label: "Max (ms)", headerClass: "alignRight"}
+                  ];
+       };
+
+       // data helpers
        $scope.getByType = function (data, type) {
            return  _.filter(data.metrics, function (m) {
                return m.type == type
@@ -112,5 +170,13 @@ dashboardApp.controller('DashboardCtrl', ["$scope",
                return item.node
            }))
        };
-    }
+
+       // formatting helpers
+       $scope.formatMs = function (value) {
+           return numeral( (value / 1000).toFixed(4) ).format('0,0.0000');
+       };
+       $scope.formatNumber = function (value) {
+           return numeral(value).format('0,0');
+       };
+   }
 ]);
